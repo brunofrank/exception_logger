6295f7e098bb4822d319fef5a13cc99ebe9d7c86
Bruno Frank Silva Cordeiro <bfscordeiro@gmail.com>
2010-11-23 09:10:08 -0200

    Fix the filters

--- assets/exception_logger.js
+++ assets/exception_logger.js
@@ -2,21 +2,27 @@ ExceptionLogger = {
   filters: ['exception_names', 'controller_actions', 'date_ranges'],
   setPage: function(num) {
     $('#page').val(num);
-    $('#query-form').submit();
+    $("#query-form").trigger('onsubmit');
+    this.clearFilters();
   },
   
   setFilter: function(context, name) {
     var filterName = '#'+ context + '_filter'
     $(filterName).val($(filterName).val() == name ? '' : name);
-    this.deselect(context, filterName);
     $('#page').val('1');
-    $('#query-form').submit();
+    $("#query-form").trigger('onsubmit');    
+    this.deselect(context, filterName);    
+    this.clearFilters();    
   },
 
   deselect: function(context, filterName) {
     $('#' + context + ' a').each(function(i, a) {
       var value = $(filterName) ? $(filterName).val() : null;
-      $(a).addClass(value && ($(a).attr('title') == value || $(a).text() == value)) ? 'selected' : '';
+      if (value && ($(a).attr('title') == value || $(a).text() == value)){
+          $(a).addClass('selected');          
+      } else {
+          $(a).removeClass('selected');                    
+      }
     });
   },
   
@@ -27,13 +33,17 @@ ExceptionLogger = {
     });
     
     return $('#query-form').serialize() + '&' + params.toQueryString('ids');    
-  }
+  },
+  
+  clearFilters: function(){
+    $.each(ExceptionLogger.filters, function(i, item){ 
+        $('#' + item + '_filter').val('');        
+    });      
+  }      
 }
 
 $(document).ready(function(){
-    $.each(ExceptionLogger.filters, function(i, item){ 
-        $('#' + item + '_filter').val('');        
-    });
+    ExceptionLogger.clearFilters();
 });
 
 Array.prototype.toQueryString =  function(name) {
@@ -48,10 +58,10 @@ Array.prototype.toQueryString =  function(name) {
     return queryString;
 }
 
-$('activity').bind("ajaxStart", function(){
-    $('activity').fadeIn(1000);
-});
+$("#activity").ajaxStart(function(){
+   $(this).fadeIn(1000);
+ });
 
-$('activity').bind("ajaxComplete", function(){
-    $('activity').fadeOut(1000);
+$('#activity').ajaxStop(function() {
+    $(this).fadeOut(1000);
 });
